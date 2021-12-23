bf24b3103828828997a3d3c39911d86b2eae83a0
BiCuSe <towoxa1296@xtrempro.com>
2021-12-23 12:08:57 +0700

    sadasdasdas

---
+++ Pages/Home.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+using WPFModernVerticalMenu.Model;
+
+namespace WPFModernVerticalMenu.Pages
+{
+    /// <summary>
+    /// Lógica de interacción para Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+
+
+        public Home()
+        {
+            InitializeComponent();
+            this.texttt();
+            this.PieChart();
+
+        }
+
+
+
+        int refq = 752;
+        int refqq = 1400;
+
+        public void texttt()
+        {
+            int summa = refq + refqq;
+            referal.Text = refq.ToString() + "  " + "Rub";
+            PC.Text = refqq.ToString()+"  "+"Rub";
+            Vseg_Summa.Text = summa.ToString() + "  " + "Rub";
+
+        }
+
+        public Func<ChartPoint, string> PointLabel { get; set; }
+
+        public void PieChart()
+        {
+            PointLabel = charPoint => string.Format("{0} Руб ", charPoint.Y, charPoint.Participation);
+            DataContext = this;
+
+        }
+
+        private void Btn_Start_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Btn_Stop_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /* private void PieChart_DataClick(object sender, LiveCharts.ChartPoint chartPoint)
+         {
+             var chart = (LiveCharts.Wpf.PieChart)chartPoint.ChartView;
+             foreach (PieSeries pieSeries in chart.Series)
+                 pieSeries.PushOut = 0;
+             var seleccionarSeries = (PieSeries)chartPoint.SeriesView;
+             seleccionarSeries.PushOut = 5;
+         }*/
+
+
+
+
+
+
+    }
+}
---
+++ Pages/Lisenze.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFModernVerticalMenu.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Lisenze.xaml
+    /// </summary>
+    public partial class Lisenze : Page
+    {
+        public Lisenze()
+        {
+            InitializeComponent();
+            BTC();
+        }
+
+        string btc = "1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2";
+
+        public void BTC()
+        {
+
+            txt_Btc.Text = btc;
+
+        }
+
+
+        private void btn_Order_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void btn_Order_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (txtlog.Text.Equals("") || txtSpon.Text.Equals(""))
+            {
+                Popup.PlacementTarget = btn_Order;
+                Popup.Placement = PlacementMode.Bottom;
+                Popup.IsOpen = true;
+                Header.PopupText.Text = "Поле логин или поле Логин спорсора не заполненно";
+            }
+        }
+
+        private void btn_Order_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+    }
+}
